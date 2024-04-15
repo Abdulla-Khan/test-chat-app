b5571a70725cf815459f9fc14a582f41ef74cc07
Abdulla-Khan <abdullahkhan.smiu@gmail.com>
2024-04-15 15:01:33 +0500

    COmmit

---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide AuthProvider;
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_demo/constants/app_constants.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'constants/color_constants.dart';
+import 'firebase_options.dart';
+import 'pages/pages.dart';
+import 'providers/providers.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // await FirebaseMessaging.instance.getInitialMessage();
+  // await PushNotificationService().setupInteractedMessage();
+
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  runApp(MyApp(prefs: prefs));
+}
+
+class MyApp extends StatelessWidget {
+  final SharedPreferences prefs;
+
+  MyApp({super.key, required this.prefs});
+
+  final _firebaseFirestore = FirebaseFirestore.instance;
+  final _firebaseStorage = FirebaseStorage.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AuthProvider>(
+          create: (_) => AuthProvider(
+            firebaseAuth: FirebaseAuth.instance,
+            googleSignIn: GoogleSignIn(),
+            prefs: prefs,
+            firebaseFirestore: _firebaseFirestore,
+          ),
+        ),
+        Provider<SettingProvider>(
+          create: (_) => SettingProvider(
+            prefs: prefs,
+            firebaseFirestore: _firebaseFirestore,
+            firebaseStorage: _firebaseStorage,
+          ),
+        ),
+        Provider<HomeProvider>(
+          create: (_) => HomeProvider(
+            firebaseFirestore: _firebaseFirestore,
+          ),
+        ),
+        Provider<ChatProvider>(
+          create: (_) => ChatProvider(
+            prefs: prefs,
+            firebaseFirestore: _firebaseFirestore,
+            firebaseStorage: _firebaseStorage,
+          ),
+        ),
+      ],
+      child: MaterialApp(
+        title: AppConstants.appTitle,
+        theme: ThemeData(
+          useMaterial3: true,
+          colorSchemeSeed: ColorConstants.themeColor,
+        ),
+        home: const SplashPage(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
